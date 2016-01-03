b4ad5da86f08aec6bac8a26de6cb53f8dccee953
Shufi <semangkaforever@gmail.com>
2016-01-03 07:51:24 +0700

    rapihin

--- SD/src/main.c
+++ SD/src/main.c
@@ -16,6 +16,7 @@ void openTap(void);
 void waterAlertOn(void);
 void waterAlertOff(void);
 void getDelayBasedOnDebit(void);
+void clearLCD(void);
 
 //USART function
 void setUpSerial();
@@ -45,6 +46,33 @@ int rainTankVolume = 0;
 int isAutoWatering = 0;
 int delay = 0;
 
+void adc_init(void) {
+	struct adc_config adc_conf;
+	struct adc_channel_config adcch_conf;
+	
+	adc_read_configuration(&MY_ADC, &adc_conf);
+	adcch_read_configuration(&MY_ADC, MY_ADC_CH, &adcch_conf);
+	
+	adc_set_conversion_parameters(&adc_conf, ADC_SIGN_OFF, ADC_RES_12, ADC_REF_VCC);
+	adc_set_conversion_trigger(&adc_conf, ADC_TRIG_MANUAL, 1, 0);
+	adc_set_clock_rate(&adc_conf, 200000UL);
+	
+	adcch_set_input(&adcch_conf, J3_PIN0, ADCCH_NEG_NONE, 1);
+	
+	adc_write_configuration(&MY_ADC, &adc_conf);
+	adcch_write_configuration(&MY_ADC, MY_ADC_CH, &adcch_conf);
+}
+
+void pwm_init(void){
+	PORTC.DIR |= PIN0_bm;
+	
+	TCC0.CTRLA = (PIN2_bm) | (PIN0_bm);
+	TCC0.CTRLB = (PIN4_bm) | (PIN2_bm) | (PIN1_bm);
+	
+	TCC0.PER = 8000;
+	TCC0.CCA = 375;
+}
+
 void setUpSerial(){
 	//Baud rate selection
 	//BSEI = (2000000/(2^0 * 16*9600) -1 = 12.0208.... ~12) ->BSALE =0
@@ -59,6 +87,7 @@ void setUpSerial(){
 	USARTC0_CTRLB= USART_TXEN_bm | USART_RXEN_bm;
 	
 }
+
 void sendChar(char c){
 	while (!(USARTC0_STATUS & USART_DREIF_bm));
 	
@@ -77,6 +106,7 @@ void sendString(char *text){
 		sendChar(*text++);
 	}
 }
+
 void receiveString()
 {
 	
@@ -88,6 +118,198 @@ void receiveString()
 	}
 }
 
+void getWaterUsageFromEEPROM(void) {
+	int i=14;
+	int Subtotal=0;
+	int total=0;
+	int index=0;
+	bool habis = false;
+	while(!habis){
+		Subtotal = nvm_eeprom_read_byte(i);
+		if(Subtotal>=255){
+			i++;
+		}
+		else{
+			habis=true;
+		}
+		total+=Subtotal;
+		Subtotal=0;
+		index = i;
+	}
+	waterUsage = nvm_eeprom_read_byte(index);
+}
+
+void setMaxWater(int max) {
+	maxWater = max;
+}
+
+void setRainTankVolume(int vol) {
+	rainTankVolume = vol;
+
+	int i = vol/255;
+	int remainder = vol%255;
+	int a;
+	for(a = 1; a <= i; a++){
+		nvm_eeprom_write_byte(a, 255);
+	}
+	
+	if(remainder!=0) {
+		nvm_eeprom_write_byte(i+1, remainder);
+	}
+	
+}
+
+static void vReceiver(void *pvParameters){
+	gpio_set_pin_high(NHD_C12832A1Z_BACKLIGHT);
+	while (1){
+		
+		char cmd = receiveChar();
+		
+		if(cmd=='a'){
+			if (isTap1Opened==0)
+			{
+				clearLCD();
+				gfx_mono_draw_string("Open Tap 1",0,0,&sysfont);
+				isTap1Opened = 1;
+			}
+			else
+			{
+				clearLCD();
+				gfx_mono_draw_string("Tap 1 had opened",0,0,&sysfont);
+			}
+			} else if(cmd=='1'){
+			if (isTap1Opened==1)
+			{
+				clearLCD();
+				gfx_mono_draw_string("Close Tap 1",0,0,&sysfont);
+				isTap1Opened = 0;
+			}
+			else
+			{
+				clearLCD();
+				gfx_mono_draw_string("Tap 1 had closed",0,0,&sysfont);
+			}
+			} else if(cmd=='b'){
+			if (isTap2Opened==0)
+			{
+				clearLCD();
+				gfx_mono_draw_string("Open Tap 2",0,0,&sysfont);
+				isTap2Opened = 1;
+			}
+			else
+			{
+				clearLCD();
+				gfx_mono_draw_string("Tap 2 had opened",0,0,&sysfont);
+			}
+			} else if(cmd=='2'){
+			if (isTap2Opened==1)
+			{
+				clearLCD();
+				gfx_mono_draw_string("Close Tap 2",0,0,&sysfont);
+				isTap2Opened = 0;
+			}
+			else
+			{
+				clearLCD();
+				gfx_mono_draw_string("Tap 2 had closed",0,0,&sysfont);
+			}
+			} else if(cmd=='c'){
+			if (isTap3Opened==0)
+			{
+				clearLCD();
+				gfx_mono_draw_string("Open Tap 3",0,0,&sysfont);
+				isTap3Opened = 1;
+			}
+			else
+			{
+				clearLCD();
+				gfx_mono_draw_string("Tap 3 had opened",0,0,&sysfont);
+			}
+			} else if(cmd=='3'){
+			if (isTap1Opened==1)
+			{
+				clearLCD();
+				gfx_mono_draw_string("Close Tap 3",0,0,&sysfont);
+				isTap3Opened = 0;
+			}
+			else
+			{
+				clearLCD();
+				gfx_mono_draw_string("Tap 3 had closed",0,0,&sysfont);
+			}
+			} else if(cmd=='d'){
+			if (isTap4Opened==0)
+			{
+				clearLCD();
+				gfx_mono_draw_string("Open Tap 4",0,0,&sysfont);
+				isTap4Opened = 1;
+			}
+			else
+			{
+				clearLCD();
+				gfx_mono_draw_string("Tap 4 had opened",0,0,&sysfont);
+			}
+			} else if(cmd=='4'){
+			if (isTap4Opened==1)
+			{
+				clearLCD();
+				gfx_mono_draw_string("Close Tap 4",0,0,&sysfont);
+				isTap4Opened = 0;
+			}
+			else
+			{
+				clearLCD();
+				gfx_mono_draw_string("Tap 4 had closed",0,0,&sysfont);
+			}
+			} else if(cmd=='e') {
+			
+			clearLCD();
+			gfx_mono_draw_string("Water Discharge",0,0,&sysfont);
+			
+			} else if(cmd=='5') {
+			
+			clearLCD();
+			gfx_mono_draw_string("Water Discharge",0,0,&sysfont);
+			
+			} else if(cmd=='f') {
+			
+			clearLCD();
+			gfx_mono_draw_string("Manual Watering",0,0,&sysfont);
+			isAutoWatering = 0;
+			
+			} else if(cmd=='6') {
+			
+			clearLCD();
+			gfx_mono_draw_string("Auto Watering",0,0,&sysfont);
+			isAutoWatering = 1;
+			
+			} else if(cmd=='g') {
+			
+			clearLCD();
+			gfx_mono_draw_string("Open Watering Tap",0,0,&sysfont);
+			if (isAutoWatering==0) {
+				isWateringTapOpened = 1;
+				} else {
+				clearLCD();
+				gfx_mono_draw_string("Auto Watering Mode ON",0,0,&sysfont);
+			}
+			
+			} else if(cmd=='7') {
+			
+			clearLCD();
+			gfx_mono_draw_string("Close Watering Tap",0,0,&sysfont);
+			isWateringTapOpened = 0;
+			
+			} else if(cmd=='p') {
+			
+			sendChar('p');
+			gfx_mono_draw_string("Send PING",0,0,&sysfont);
+			
+		}
+		vTaskDelay(1);
+		
+	}
+}
 
 static void openCloseTap(void *pvParameters)
 {
@@ -137,117 +359,87 @@ static void openCloseTap(void *pvParameters)
 	 }
 }
 
-void setMaxWater(int max) {
-	maxWater = max;
-}
-
-void setRainTankVolume(int vol) {
-	rainTankVolume = vol;
-
-	int i = vol/255;
-	int remainder = vol%255;
-	int a;
-	for(a = 1; a <= i; a++){
-		nvm_eeprom_write_byte(a, 255);
-	}
-	
-	if(remainder!=0) {
-		nvm_eeprom_write_byte(i+1, remainder);
-	}
-	
-}
-
-//Mengatur debit air (potensiometer)
-void adc_init(void) {
-	struct adc_config adc_conf;
-	struct adc_channel_config adcch_conf;
-	
-	adc_read_configuration(&MY_ADC, &adc_conf);
-	adcch_read_configuration(&MY_ADC, MY_ADC_CH, &adcch_conf);
-	
-	adc_set_conversion_parameters(&adc_conf, ADC_SIGN_OFF, ADC_RES_12, ADC_REF_VCC);
-	adc_set_conversion_trigger(&adc_conf, ADC_TRIG_MANUAL, 1, 0);
-	adc_set_clock_rate(&adc_conf, 200000UL);
-	
-	adcch_set_input(&adcch_conf, J3_PIN0, ADCCH_NEG_NONE, 1);
-	
-	adc_write_configuration(&MY_ADC, &adc_conf);
-	adcch_write_configuration(&MY_ADC, MY_ADC_CH, &adcch_conf);
-}
-
-static void setWaterDebit(void *pvParameters)
-{
-	while (1) {
+static void checkTap(void *pvParameters) {
+	while(1) {
+		if(isTap1Opened == 0) {
+			LED_Off(LED0);
+			} else {
+			LED_On(LED0);
+		}
 		
-		//uint16_t result;
-		adc_enable(&MY_ADC);
-		adc_start_conversion(&MY_ADC, MY_ADC_CH);
-		adc_wait_for_interrupt_flag(&MY_ADC, MY_ADC_CH);
-		debit = adc_get_result(&MY_ADC, MY_ADC_CH)/1000;
-		vTaskDelay(500);
+		if(isTap2Opened == 0) {
+			LED_Off(LED1);
+			} else {
+			LED_On(LED1);
+		}
 		
+		if(isTap3Opened == 0) {
+			LED_Off(LED2);
+			} else {
+			LED_On(LED2);
+		}
+		
+		if(isTap4Opened == 0) {
+			LED_Off(LED3);
+			} else {
+			LED_On(LED3);
+		}
+		
+		if(isWateringTapOpened == 0) {
+			LED_On(LED0);
+			LED_On(LED1);
+			} else {
+			LED_Off(LED0);
+			LED_Off(LED1);
+		}
+		
+		vTaskDelay(1);
 	}
 }
 
-//Watering from rain tank
 static void watering(void *vpParameters) {
 	while(1) {
 		if(isAutoWatering==1 && lightIntensity>=150  && lightIntensity<=200) {
-				
+			
 			int wateringVol = 500;
 			isWateringTapOpened = 1;	//triggering openCloseTap()
-				
+			
 			while(wateringVol>=0) {
 				wateringVol--;
 				vTaskDelay(5);
 			}
-				
+			
 			rainTankVolume -= wateringVol;
 			setRainTankVolume(rainTankVolume);
 			isWateringTapOpened=0;
-				
-		} else if(isAutoWatering==0 && isWateringTapOpened==1) {
-				
+			
+			} else if(isAutoWatering==0 && isWateringTapOpened==1) {
+			
 			while(rainTankVolume>=0 && isWateringTapOpened==1) {
 				rainTankVolume--;
 				vTaskDelay(5);
 			}
 			
 			setRainTankVolume(rainTankVolume);
-				
+			
 		}
 	}
 	vTaskDelay(1);
-} 
-
-void getWaterUsageFromEEPROM(void) {
-	int i=14;
-	int Subtotal=0;
-	int total=0;
-	int index=0;
-	bool habis = false;
-	while(!habis){
-		Subtotal = nvm_eeprom_read_byte(i);
-		if(Subtotal>=255){
-			i++;
-		}
-		else{
-			habis=true;
-		}
-		total+=Subtotal;
-		Subtotal=0;
-		index = i;
-	}
-	waterUsage = nvm_eeprom_read_byte(index);
 }
 
-//Menghitung penggunaan air (eeprom)
-void getDelayBasedOnDebit(void) {
-	if(debit==0) delay = 250;
-	else if(debit==1) delay = 200;
-	else if(debit==2) delay = 150;
-	else if(debit==3) delay = 100;
-	else if(debit==4) delay = 50;
+static void checkLight(void *pvParameters)
+{
+	while(1)
+	{
+		// Start an ADC conversion of the lightsensor
+		lightsensor_measure(); //memulai membaca nilai sensor cahaya dari adc
+		if(lightsensor_data_is_ready()) //jika nilainya sensor cahaysa sudah didapat
+		{
+			lightIntensity = lightsensor_get_raw_value(); //membaca nilai sensor cahaya
+		}
+		vTaskDelay(10);
+	}
+	
 }
 
 static void countWaterUsage(void *vpParameters) {
@@ -278,47 +470,26 @@ static void countWaterUsage(void *vpParameters) {
 	}
 }
 
-
-
-//Membuka dan menutup keran (servo)
-void pwm_init(void){
-	PORTC.DIR |= PIN0_bm;
-	
-	TCC0.CTRLA = (PIN2_bm) | (PIN0_bm);
-	TCC0.CTRLB = (PIN4_bm) | (PIN2_bm) | (PIN1_bm);
-	
-	TCC0.PER = 8000;
-	TCC0.CCA = 375;
+static void setWaterDebit(void *pvParameters)
+{
+	while (1) {
+		
+		//uint16_t result;
+		adc_enable(&MY_ADC);
+		adc_start_conversion(&MY_ADC, MY_ADC_CH);
+		adc_wait_for_interrupt_flag(&MY_ADC, MY_ADC_CH);
+		debit = adc_get_result(&MY_ADC, MY_ADC_CH)/1000;
+		vTaskDelay(500);
+		
+	}
 }
 
-void closeTap(void){
-	PORTC.DIR |= PIN0_bm;
-	
-	TCC0.CTRLA = (PIN2_bm) | (PIN0_bm);
-	TCC0.CTRLB = (PIN4_bm) | (PIN2_bm) | (PIN1_bm);
-	
-	TCC0.PER = 8000;
-	TCC0.CCA = 1;
-}
-void openTap(void){
-	PORTC.DIR |= PIN0_bm;
-	
-	TCC0.CTRLA = (PIN2_bm) | (PIN0_bm);
-	TCC0.CTRLB = (PIN4_bm) | (PIN2_bm) | (PIN1_bm);
-	
-	TCC0.PER = 8000;
-	TCC0.CCA = 375;
-}
-
-//Memberi peringatan setiap menggunakan air saat penggunaan air sudah mencapai batas maksimal (buzzer)
-void waterAlertOn(void) {
-	PORTE_DIR=0b00000001;
-	PORTE_OUT=0b00000001;
-}
-
-void waterAlertOff(void) {
-	PORTE_DIR=0b00000001;
-	PORTE_OUT=0b00000000;
+void getDelayBasedOnDebit(void) {
+	if(debit==0) delay = 250;
+	else if(debit==1) delay = 200;
+	else if(debit==2) delay = 150;
+	else if(debit==3) delay = 100;
+	else if(debit==4) delay = 50;
 }
 
 static void waterAlertTask(void *pvParameters) {
@@ -333,217 +504,40 @@ static void waterAlertTask(void *pvParameters) {
 	}
 }
 
-//Mengukur intensitas cahaya
-static void checkLight(void *pvParameters)
-{
-	while(1)
-	{
-		// Start an ADC conversion of the lightsensor
-		lightsensor_measure(); //memulai membaca nilai sensor cahaya dari adc
-		if(lightsensor_data_is_ready()) //jika nilainya sensor cahaysa sudah didapat
-		{
-			lightIntensity = lightsensor_get_raw_value(); //membaca nilai sensor cahaya
-		}
-		vTaskDelay(10);
-	}
+void closeTap(void){
+	PORTC.DIR |= PIN0_bm;
 	
+	TCC0.CTRLA = (PIN2_bm) | (PIN0_bm);
+	TCC0.CTRLB = (PIN4_bm) | (PIN2_bm) | (PIN1_bm);
+	
+	TCC0.PER = 8000;
+	TCC0.CCA = 1;
 }
 
+void openTap(void){
+	PORTC.DIR |= PIN0_bm;
+	
+	TCC0.CTRLA = (PIN2_bm) | (PIN0_bm);
+	TCC0.CTRLB = (PIN4_bm) | (PIN2_bm) | (PIN1_bm);
+	
+	TCC0.PER = 8000;
+	TCC0.CCA = 375;
+}
+
+void waterAlertOn(void) {
+	PORTE_DIR=0b00000001;
+	PORTE_OUT=0b00000001;
+}
+
+void waterAlertOff(void) {
+	PORTE_DIR=0b00000001;
+	PORTE_OUT=0b00000000;
+}
 
 void clearLCD(void){
 	gfx_mono_draw_string("                    ",0,0,&sysfont);
 }
 
-//tanda kerannya terbuka atau tertutup (led)
-static void checkTap(void *pvParameters) {
-	while(1) {
-		if(isTap1Opened == 0) {
-			LED_Off(LED0);
-		} else {
-			LED_On(LED0);
-		}
-		
-		if(isTap2Opened == 0) {
-			LED_Off(LED1);
-		} else {
-			LED_On(LED1);
-		}
-		
-		if(isTap3Opened == 0) {
-			LED_Off(LED2);
-		} else {
-			LED_On(LED2);
-		}
-		
-		if(isTap4Opened == 0) {
-			LED_Off(LED3);
-			} else {
-			LED_On(LED3);
-		}
-		
-		if(isWateringTapOpened == 0) {
-			LED_On(LED0);
-			LED_On(LED1);
-		} else {
-			LED_Off(LED0);
-			LED_Off(LED1);
-		}
-		
-		vTaskDelay(1);
-	}
-}
-
-static void vReceiver(void *pvParameters){
-	gpio_set_pin_high(NHD_C12832A1Z_BACKLIGHT);
-	while (1){
-		
-		char cmd = receiveChar();
-		
-		if(cmd=='a'){
-			if (isTap1Opened==0)
-			{
-				clearLCD();
-				gfx_mono_draw_string("Open Tap 1",0,0,&sysfont);
-				isTap1Opened = 1;
-			} 
-			else
-			{
-				clearLCD();
-				gfx_mono_draw_string("Tap 1 had opened",0,0,&sysfont);
-			}
-		} else if(cmd=='1'){
-			if (isTap1Opened==1)
-			{
-				clearLCD();
-				gfx_mono_draw_string("Close Tap 1",0,0,&sysfont);
-				isTap1Opened = 0;
-			} 
-			else
-			{
-				clearLCD();
-				gfx_mono_draw_string("Tap 1 had closed",0,0,&sysfont);
-			}
-		} else if(cmd=='b'){
-			if (isTap2Opened==0)
-			{
-				clearLCD();
-				gfx_mono_draw_string("Open Tap 2",0,0,&sysfont);
-				isTap2Opened = 1;
-			}
-			else
-			{
-				clearLCD();
-				gfx_mono_draw_string("Tap 2 had opened",0,0,&sysfont);
-			}
-		} else if(cmd=='2'){
-			if (isTap2Opened==1)
-			{
-				clearLCD();
-				gfx_mono_draw_string("Close Tap 2",0,0,&sysfont);
-				isTap2Opened = 0;
-			}
-			else
-			{
-				clearLCD();
-				gfx_mono_draw_string("Tap 2 had closed",0,0,&sysfont);
-			}
-		} else if(cmd=='c'){
-			if (isTap3Opened==0)
-			{
-				clearLCD();
-				gfx_mono_draw_string("Open Tap 3",0,0,&sysfont);
-				isTap3Opened = 1;
-			}
-			else
-			{
-				clearLCD();
-				gfx_mono_draw_string("Tap 3 had opened",0,0,&sysfont);
-			}
-		} else if(cmd=='3'){
-			if (isTap1Opened==1)
-			{
-				clearLCD();
-				gfx_mono_draw_string("Close Tap 3",0,0,&sysfont);
-				isTap3Opened = 0;
-			}
-			else
-			{
-				clearLCD();
-				gfx_mono_draw_string("Tap 3 had closed",0,0,&sysfont);
-			}
-		} else if(cmd=='d'){
-			if (isTap4Opened==0)
-			{
-				clearLCD();
-				gfx_mono_draw_string("Open Tap 4",0,0,&sysfont);
-				isTap4Opened = 1;
-			}
-			else
-			{
-				clearLCD();
-				gfx_mono_draw_string("Tap 4 had opened",0,0,&sysfont);
-			}
-		} else if(cmd=='4'){
-			if (isTap4Opened==1)
-			{
-				clearLCD();
-				gfx_mono_draw_string("Close Tap 4",0,0,&sysfont);
-				isTap4Opened = 0;
-			}
-			else
-			{
-				clearLCD();
-				gfx_mono_draw_string("Tap 4 had closed",0,0,&sysfont);
-			}
-		} else if(cmd=='e') {
-			
-			clearLCD();
-			gfx_mono_draw_string("Water Discharge",0,0,&sysfont);
-			
-		} else if(cmd=='5') {
-			
-			clearLCD();
-			gfx_mono_draw_string("Water Discharge",0,0,&sysfont);
-			
-		} else if(cmd=='f') {
-			
-			clearLCD();
-			gfx_mono_draw_string("Manual Watering",0,0,&sysfont);
-			isAutoWatering = 0;
-			
-		} else if(cmd=='6') {
-			
-			clearLCD();
-			gfx_mono_draw_string("Auto Watering",0,0,&sysfont);
-			isAutoWatering = 1;
-			
-		} else if(cmd=='g') {
-			
-			clearLCD();
-			gfx_mono_draw_string("Open Watering Tap",0,0,&sysfont);
-			if (isAutoWatering==0) {
-				isWateringTapOpened = 1;
-			} else {
-				clearLCD();
-				gfx_mono_draw_string("Auto Watering Mode ON",0,0,&sysfont);
-			}
-			
-		} else if(cmd=='7') {
-			
-			clearLCD();
-			gfx_mono_draw_string("Close Watering Tap",0,0,&sysfont);
-			isWateringTapOpened = 0;
-			
-		} else if(cmd=='p') {
-			
-			sendChar('p');
-			gfx_mono_draw_string("Send PING",0,0,&sysfont);
-			
-		}
-		vTaskDelay(1);
-		
-	}
-}
 
 int main (void)
 {
